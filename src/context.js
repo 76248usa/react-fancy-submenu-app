89261a76ff42a02eaa39aa4ed38053921fa5e0db
Elsabe Crous <elsabecrous@Elsabes-MBP.lan>
2023-02-17 17:43:56 -0600

    first commit

---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({page:'', links: []})
+
+    const openSideBar = () => {
+       setIsSideBarOpen(false) 
+    }
+    const closeSideBar = () => {
+        setIsSideBarOpen(false) 
+    }
+     const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page);
+        setLocation(coordinates)
+        setIsSubmenuOpen(true) 
+    }
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false) 
+    }
+    return <AppContext.Provider value={{
+        isSubmenuOpen,isSideBarOpen,openSideBar,
+        closeSubmenu,closeSideBar, openSubmenu,
+        location, page
+    }}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
